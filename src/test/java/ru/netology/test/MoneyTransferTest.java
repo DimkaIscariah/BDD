7a6529fc7498c2ca8ca1d0edea3b774395450410
Dmitry <once87@yandex.ru>
2023-07-02 19:49:36 +0300

    Update

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -25,6 +25,53 @@ class MoneyTransferTest {
 
     @Test
     void transferFromCardToCard() {
+        var firstCardNumber = getOneCardNumber();
+        var twoCardNumber = getTwoCardNumber();
+        var oneCardBalance = dashboardPage.getCardBalance(0);
+        var twoCardBalance = dashboardPage.getCardBalance(1);
+        var amount = 1000;
+        var expectedBalanceOneCard = oneCardBalance - amount;
+        var expectedBalanceTwoCard = twoCardBalance + amount;
+        var transferPages = dashboardPage.selectCardToTransfer(twoCardNumber);
+        dashboardPage = transferPages.setRefill(1000,getOneCardNumber());
+        var actualBalanceOneCard = dashboardPage.getCardBalance(0);
+        var actualBalanceTwoCard = dashboardPage.getCardBalance(1);
+        assertEquals(expectedBalanceOneCard, actualBalanceOneCard);
+        assertEquals(expectedBalanceTwoCard ,actualBalanceTwoCard);
 
+    }
+
+    @Test
+    void transferFromCardToCard2() {
+        var firstCardNumber = getOneCardNumber();
+        var twoCardNumber = getTwoCardNumber();
+        var oneCardBalance = dashboardPage.getCardBalance(0);
+        var twoCardBalance = dashboardPage.getCardBalance(1);
+        var amount = 10000;
+        var expectedBalanceOneCard = oneCardBalance - amount;
+        var expectedBalanceTwoCard = twoCardBalance + amount;
+        var transferPages = dashboardPage.selectCardToTransfer(twoCardNumber);
+        dashboardPage = transferPages.setRefill(10000,getOneCardNumber());
+        var actualBalanceOneCard = dashboardPage.getCardBalance(0);
+        var actualBalanceTwoCard = dashboardPage.getCardBalance(1);
+        assertEquals(expectedBalanceOneCard, actualBalanceOneCard);
+        assertEquals(expectedBalanceTwoCard ,actualBalanceTwoCard);
+    }
+
+    @Test
+    void transferFromCardToCard3() {
+        var firstCardNumber = getOneCardNumber();
+        var twoCardNumber = getTwoCardNumber();
+        var oneCardBalance = dashboardPage.getCardBalance(0);
+        var twoCardBalance = dashboardPage.getCardBalance(1);
+        var amount = 2500;
+        var expectedBalanceOneCard = oneCardBalance - amount;
+        var expectedBalanceTwoCard = twoCardBalance + amount;
+        var transferPages = dashboardPage.selectCardToTransfer(twoCardNumber);
+        dashboardPage = transferPages.setRefill(2500,getOneCardNumber());
+        var actualBalanceOneCard = dashboardPage.getCardBalance(0);
+        var actualBalanceTwoCard = dashboardPage.getCardBalance(1);
+        assertEquals(expectedBalanceOneCard, actualBalanceOneCard);
+        assertEquals(expectedBalanceTwoCard ,actualBalanceTwoCard);
     }
 }
